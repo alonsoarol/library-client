2dacbb20fc83a26b6d182de1535d5b0cbe69ad52
Arol <87845741+alonsoarol@users.noreply.github.com>
2022-10-23 23:07:10 -0300

    Revert "LBC-1 creamos las paginas y configuramos react-router"

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,7 @@
 import "./styles/App.css";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
-import { Home } from "./page/Home";
-import { Customers } from "./page/Customers";
-import { Providers } from "./page/Providers";
-import { Sales } from "./page/Sales";
-import { Inventory } from "./page/Inventory";
 
 function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/" index element={<Home />} />
-        <Route path="/customers" element={<Customers />} />
-        <Route path="/providers" element={<Providers />} />
-        <Route path="/sales" element={<Sales />} />
-        <Route path="/inventory" element={<Inventory />} />
-        <Route path="/*" element={<h1>404 Not Found</h1>} />
-      </Routes>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
