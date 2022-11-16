7ff1295c42e6e21749d2aab080867231dc2aa736
HelheimARG <leodlv6@gmail.com>
2022-11-16 16:41:57 -0300

    LBC-16 diseño de la pagina home

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,6 +1,8 @@
 import { PageBanner } from "../components/PageBanner";
 import { RiCustomerService2Line } from "react-icons/ri";
 import { useLoginStore } from "../context/loginStore";
+import { Fab, Stack, Typography } from "@mui/material";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import { GoBack } from "../components/common/GoBack";
 
 import { useState } from "react";
@@ -10,16 +12,25 @@ export const Home = () => {
 
   return (
     <>
-      <PageBanner title="Home" icon={<RiCustomerService2Line />} />
-
-      <div className="appBody">
-        <GoBack />
-        {UserData && (
-          <h3 style={{ textAlign: "center" }}>
-            Welcome Back {UserData.name} to LibraryJS.
-          </h3>
-        )}
-      </div>
+      <PageBanner title="Home" icon={<HomeOutlinedIcon />} />
+      <Stack
+        gridArea="appbody"
+        justifyContent="center"
+        alignItems="center"
+        sx={{
+          background: "linear-gradient(to right, #33373E, #F1F3F7)",
+        }}
+      >
+        <Typography variant="h1"  fontWeight={700} > Welcome {UserData.name} </Typography>
+        <Typography variant="h1"  fontWeight={700} > to </Typography>
+          <Stack
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <img src="/Logo.png" className="logo-home" width={600} />
+          </Stack> 
+        </Stack>
     </>
   );
 };
